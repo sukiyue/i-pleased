9fba5ccba7a34a46d404bc92c47b2992fe886bbc
qiujunjie <junjie.qiu@wcompass.com>
2022-10-31 17:09:22 +0800

    add ES6 Module

--- docs/.vitepress/config/nav/index.ts
+++ docs/.vitepress/config/nav/index.ts
@@ -7,6 +7,6 @@ import { DefaultTheme } from "vitepress";
 export const nav: DefaultTheme.Config['nav'] = [ 
   { text: 'Guide', link: '/guide/' },
   {
-    text: 'Document', link: '/document/javascript/JS-Array'
+    text: 'Note', link: '/document/javascript/JS-Array'
   }
 ]
